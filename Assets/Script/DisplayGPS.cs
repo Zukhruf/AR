2dd3121bd024523f9155ed5cfb591c2f3e5b3fef
ZukhrufMifta <zukhruf.m.f@gmail.com>
2018-05-06 21:45:15 +0700

    Simple Vuforia SDK Augmented Reality

---
+++ Assets/Script/DisplayGPS.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DisplayGPS : MonoBehaviour {
+
+    public Text displayGPS;
+	
+	// Update is called once per frame
+	void Update () {
+        displayGPS.text = "Lat : " + GPS.Instance.latitude.ToString() + "  Lon : " + GPS.Instance.longitude.ToString();
+    }
+}
---
+++ Assets/Script/DisplayWatch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class DisplayWatch : MonoBehaviour {
+
+    public Text Jam;
+	
+	
+	// Update is called once per frame
+	void Update () {
+
+        DateTime waktu = DateTime.Now;
+        String jam = waktu.Hour.ToString().PadLeft(2, '0');
+        String menit = waktu.Minute.ToString().PadLeft(2, '0');
+        String detik = waktu.Second.ToString().PadLeft(2, '0');
+
+        Jam.text = jam + ":" + menit + ":" + detik;
+
+    }
+}
